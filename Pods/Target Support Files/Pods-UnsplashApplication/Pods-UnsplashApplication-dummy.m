3b330c66712198298895fa5013db945cdb8c81c4
BaKarlan <73016991+DaniilBagautdinov@users.noreply.github.com>
2022-10-06 23:57:07 +0300

    Delete Pods directory

--- Pods/Target Support Files/Pods-UnsplashApplication/Pods-UnsplashApplication-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UnsplashApplication : NSObject
-@end
-@implementation PodsDummy_Pods_UnsplashApplication
-@end
--- Pods/Target Support Files/UnsplashPhotoPicker/UnsplashPhotoPicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UnsplashPhotoPicker : NSObject
-@end
-@implementation PodsDummy_UnsplashPhotoPicker
-@end
